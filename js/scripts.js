537f11c0ca1a24b51ab81a915143b1730afcb669
linnango <106789295+linnango@users.noreply.github.com>
2022-06-18 14:31:31 -0400

    add hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
 console.log("Hello, welcome to my self-made portfolio site!")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+ } else {
+   x.className = 'navtoggle';
+ }
+}
