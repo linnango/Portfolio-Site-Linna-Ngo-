270251fe92a30ea8ca6abc44884fdf712eb3186f
linnango <106789295+linnango@users.noreply.github.com>
2022-06-19 21:52:28 -0400

    fix script to add mobile header and fix spacing below header

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 console.log("Hello, welcome to my self-made portfolio site!")
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
   x.className += ' responsive';
  } else {
    x.className = 'navtoggle';
